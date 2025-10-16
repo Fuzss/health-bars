11fd80d5be44e29cf11a606207851201e2beb4e1
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-16 09:54:48 +0200

    update for latest puzzles lib

--- 1.21.10/Fabric/src/main/java/fuzs/healthbars/fabric/client/HealthBarsFabricClient.java
+++ 1.21.10/Fabric/src/main/java/fuzs/healthbars/fabric/client/HealthBarsFabricClient.java
@@ -2,17 +2,13 @@ package fuzs.healthbars.fabric.client;
 
 import fuzs.healthbars.HealthBars;
 import fuzs.healthbars.client.HealthBarsClient;
-import fuzs.healthbars.client.particle.DamageValueParticleGroup;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.particle.v1.ParticleRendererRegistry;
 
 public class HealthBarsFabricClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         ClientModConstructor.construct(HealthBars.MOD_ID, HealthBarsClient::new);
-        // TODO move this to Puzzles Lib
-        ParticleRendererRegistry.register(DamageValueParticleGroup.GROUP, DamageValueParticleGroup::new);
     }
 }
